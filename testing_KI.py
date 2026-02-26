d4a8cf6339a437f0cf3a4cf2579d648ac5d7a034
aksel <aksel.vaaler@ntnu.no>
2026-02-26 12:32:49 +0100

    minmal testing changes

--- testing_KI.py
+++ testing_KI.py
@@ -3,33 +3,46 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 import pytest
+from dataclasses import dataclass
 from flax import nnx
 
 from openpi.models import pi0_config, model as _model
+from openpi.training import config as _config
 from openpi.models.pi0 import Pi0 
 
+
+@dataclass
 class TestingParameters:
     """Class to hold testing parameters for easy modification."""
-    def __init__(self):
-        self.batch_size = 2
-        self.seq_len = 64
-        self.action_dim = 8
-        self.action_horizon = 15
-        self.num_joints = 7
-        self.image_size = (224, 224, 3)
-        self.action_dims = 8
+    batch_size: int = 2
+    seq_len: int = 64
+    action_dim: int = 8
+    action_horizon: int = 15
+    num_joints: int = 7
+    image_size: tuple = (224, 224, 3)
+    action_dims: int = 8
+    config_name: str = "debug_pi05" 
+    
+# Create a global instance
+testing_parameters = TestingParameters()
 
-@pytest.fixture
-def testing_parameters(): 
-    return TestingParameters()
 
 
 def create_dummy_observation_DROID() -> dict : 
-
-    return {"test":testing_parameters.image_size}
+    """Creates a random input example for the Droid policy."""
+    return {
+        "observation/exterior_image_1_left": np.random.randint(256, size=(224, 224, 3), dtype=np.uint8),
+        "observation/wrist_image_left": np.random.randint(256, size=(224, 224, 3), dtype=np.uint8),
+        "observation/joint_position": np.random.rand(7),
+        "observation/gripper_position": np.random.rand(1),
+        "prompt": "do something",
+    }
 
 if __name__ == "__main__":
     
-    print(create_dummy_observation_DROID())
+    config = _config.get_config(testing_parameters.config_name)
+
+    print(config)
+    
 
      
